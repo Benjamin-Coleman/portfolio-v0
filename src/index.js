eb30ca5bbcba77c3cf1190b9554693dd5422b30a
Benjamin-Coleman <hello@benjamincoleman.me>
2017-12-02 12:56:50 -0500

    minor css changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
+import './Fonts.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
